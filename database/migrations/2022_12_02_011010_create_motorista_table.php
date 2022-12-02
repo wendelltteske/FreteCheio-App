26f349c9673f3c1a294629db66a9c08754b4a0b1
Guilherme Vilela <68655658+Guilherme-Vilela@users.noreply.github.com>
2022-12-01 22:24:59 -0300

    criacao tabelas relacionadas ao caminhao

---
+++ database/migrations/2022_12_02_011010_create_motorista_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('motorista', function (Blueprint $table) {
+            $table->id();
+            $table->string("nome")->notnull();
+            $table->string("cpf/cnpj")->notnull();
+            $table->string("numero_cnh")->notnull();
+            $table->string("telefone")->notnull();
+            $table->string("numero_cnh")->notnull();
+            $table->string("foto_frontal_cnh")->nullable();
+            $table->string("foto_traseira_cnh")->nullable();
+            $table->string("foto_pessoal")->nullable();
+            $table->string("status")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('motorista');
+    }
+};
